9eaeb7a82f539efd88f6e5347b03d1dd1d4dfc75
Sukh8750 <Sukhchain_Sidhu@ymail.com>
2017-06-03 21:50:50 +0530

    commit6

---
+++ src/NewFeature.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class NewFeature{
+public static void main(String... args) {
+	
+}
